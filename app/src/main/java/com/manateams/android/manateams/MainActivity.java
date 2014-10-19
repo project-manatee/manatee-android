48fe39c365d3920ad0b1049d971fd788dd45d2c5
patil215 <neilpatil215@gmail.com>
2014-10-18 21:51:11 -0500

    Primary colors and app theme

--- app/src/main/java/com/manateams/android/manateams/MainActivity.java
+++ app/src/main/java/com/manateams/android/manateams/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    /* Solely used for login. If the user is already logged in, forwards to showing the grades. */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
