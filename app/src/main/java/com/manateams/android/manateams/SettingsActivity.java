bbad95783df1152a10a1c25c77e9488fe3d7d7aa
patil215 <neilpatil215@gmail.com>
2014-11-08 14:53:56 -0600

    Calculate weighted GPA with weighted and excluded classes

--- app/src/main/java/com/manateams/android/manateams/SettingsActivity.java
+++ app/src/main/java/com/manateams/android/manateams/SettingsActivity.java
@@ -1,9 +1,13 @@
 package com.manateams.android.manateams;
 
 import android.os.Bundle;
+import android.preference.MultiSelectListPreference;
 import android.preference.PreferenceActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.manateams.android.manateams.util.DataManager;
+import com.quickhac.common.data.Course;
 
 
 public class SettingsActivity extends PreferenceActivity {
@@ -12,6 +16,21 @@ public class SettingsActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.layout.activity_settings);
+
+        MultiSelectListPreference weightedPreference = (MultiSelectListPreference) findPreference("pref_weightedClasses");
+
+        MultiSelectListPreference excludedPreference = (MultiSelectListPreference) findPreference("pref_excludedClasses");
+
+        Course[] courses = new DataManager(this).getCourseGrades();
+
+        CharSequence[] classes = new CharSequence[courses.length];
+        for (int i = 0; i < courses.length; i++) {
+            classes[i] = courses[i].title;
+        }
+        weightedPreference.setEntryValues(classes);
+        weightedPreference.setEntries(classes);
+        excludedPreference.setEntryValues(classes);
+        excludedPreference.setEntries(classes);
     }
 
 
--- app/src/main/java/com/manateams/android/manateams/fragments/GPAFragment.java
+++ app/src/main/java/com/manateams/android/manateams/fragments/GPAFragment.java
@@ -1,6 +1,8 @@
 package com.manateams.android.manateams.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,10 @@ import com.manateams.android.manateams.util.DataManager;
 import com.quickhac.common.GPACalc;
 import com.quickhac.common.data.Course;
 import com.quickhac.common.util.Numeric;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class GPAFragment extends Fragment {
 
@@ -47,12 +53,67 @@ public class GPAFragment extends Fragment {
 
     private void populateGPASummaries() {
         if(courses != null) {
-            TextView titleText = new TextView(getActivity());
-            titleText.setText(getString(R.string.text_gpa_unweighted));
+            TextView unweightedTitle = new TextView(getActivity());
+            unweightedTitle.setText(getString(R.string.text_gpa_unweighted));
             TextView unweightedText = new TextView(getActivity());
             unweightedText.setText(String.valueOf(Numeric.round(GPACalc.unweighted(courses), 3)));
-            rootLayout.addView(titleText);
+            rootLayout.addView(unweightedTitle);
             rootLayout.addView(unweightedText);
+
+
+            TextView weightedTitle = new TextView(getActivity());
+            weightedTitle.setText(getString(R.string.text_gpa_weighted));
+            TextView weightedText = new TextView(getActivity());
+            weightedText.setText(String.valueOf(Numeric.round(getWeightedGPA(), 3)));
+            rootLayout.addView(weightedTitle);
+            rootLayout.addView(weightedText);
         }
+    }
+
+    private double getWeightedGPA() {
+        SharedPreferences defaultPrefs = PreferenceManager
+                .getDefaultSharedPreferences(getActivity());
+
+        List<String> weightedClasses = new ArrayList<String>();
+        Set<String> savedWeighted = defaultPrefs.getStringSet(
+                "pref_weightedClasses", null);
+        if (savedWeighted != null) {
+            String[] weighted = savedWeighted
+                    .toArray(new String[savedWeighted.size()]);
+            if (weighted != null) {
+                for (int i = 0; i < weighted.length; i++) {
+                    weightedClasses.add(weighted[i]);
+                }
+            }
+        }
+
+        List<String> excludedClasses = new ArrayList<String>();
+        Set<String> savedExcluded = defaultPrefs.getStringSet(
+                "pref_excludedClasses", null);
+        if (savedExcluded != null) {
+            String[] excluded = savedExcluded
+                    .toArray(new String[savedExcluded.size()]);
+            if (excluded != null) {
+                for (int i = 0; i < excluded.length; i++) {
+                    excludedClasses.add(excluded[i]);
+                }
+            }
+        }
+
+        // remove excluded classes from list of weighed classes to calculate
+        List<String> toWeighted = new ArrayList<String>();
+        for (int i = 0; i < weightedClasses.size(); i++) {
+            boolean excluded = false;
+            for (int d = 0; d < excludedClasses.size(); d++) {
+                if (d < excludedClasses.size()
+                        && excludedClasses.get(d).equals(courses[i].title)) {
+                    excluded = true;
+                }
+            }
+            if (!excluded) {
+                toWeighted.add(weightedClasses.get(i));
+            }
+        }
+        return GPACalc.weighted(courses, toWeighted, 0);
     }
 }
