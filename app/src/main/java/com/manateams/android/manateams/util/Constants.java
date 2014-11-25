3cda72904c11145ef6f40143e8d67ecbdd358757
patil215 <neilpatil215@gmail.com>
2014-11-25 09:47:14 -0600

    Change colors to rainbow

--- app/src/main/java/com/manateams/android/manateams/util/Constants.java
+++ app/src/main/java/com/manateams/android/manateams/util/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String EXTRA_GRADES = "grades";
     public static final String EXTRA_COURSEINDEX = "courseIndex";
     public static final String EXTRA_COURSETITLE = "courseTitle";
-    public static final String[] COLORS = {"#e51c23", "#3f51b5", "#259b24", "#ff9800", "#607d8b", "#5677fc", "#8bc34a", "#009688", "#e91e63"};
+    public static final String[] COLORS = {"#e91e63", "#e51c23", "#ff9800", "#8bc34a", "#259b24", "#009688", "#5677fc", "#3f61b5", "#607d8b"};
     public static final String FILE_COURSES = "courses";
     public static final String FILE_BASE_CLASSGRADES = "classgrades";
     public static final long INTERVAL_GRADE_SCRAPE = 60000; // 60 seconds
