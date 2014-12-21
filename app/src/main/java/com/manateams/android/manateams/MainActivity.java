1d0ca7b455eb754fdb1a761157d6127933997ae4
patil215 <neilpatil215@gmail.com>
2014-12-20 20:10:45 -0600

    Disable edittexts and buttons when logging in

--- app/src/main/java/com/manateams/android/manateams/MainActivity.java
+++ app/src/main/java/com/manateams/android/manateams/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,6 +38,7 @@ public class MainActivity extends ActionBarActivity implements AsyncTaskComplete
     private TextView usernameText;
     private TextView passwordText;
     private TextView studentIdText;
+    private Button loginButton;
 
     private DataManager dataManager;
 
@@ -71,6 +73,7 @@ public class MainActivity extends ActionBarActivity implements AsyncTaskComplete
         usernameText = (TextView) findViewById(R.id.edittext_login_username);
         passwordText = (TextView) findViewById(R.id.edittext_login_password);
         studentIdText = (TextView) findViewById(R.id.edittext_login_id);
+        loginButton = (Button) findViewById(R.id.button_login);
     }
 
     @Override
@@ -105,6 +108,23 @@ public class MainActivity extends ActionBarActivity implements AsyncTaskComplete
                 username = usernameText.getText().toString();
                 password = passwordText.getText().toString();
                 studentId = studentIdText.getText().toString();
+                // Annoying programmatic UI code
+                usernameText.setFocusable(false);
+                usernameText.setFocusableInTouchMode(false);
+                usernameText.setClickable(false);
+                usernameText.setEnabled(false);
+                passwordText.setFocusable(false);
+                passwordText.setFocusableInTouchMode(false);
+                passwordText.setClickable(false);
+                passwordText.setEnabled(false);
+                studentIdText.setFocusable(false);
+                studentIdText.setFocusableInTouchMode(false);
+                studentIdText.setClickable(false);
+                studentIdText.setEnabled(false);
+                loginButton.setFocusable(false);
+                loginButton.setFocusableInTouchMode(false);
+                loginButton.setClickable(false);
+                loginButton.setEnabled(false);
                 new CourseLoadTask(this, this).execute(username, password, studentId);
             } else {
                 Toast.makeText(this, getString(R.string.toast_fill_info), Toast.LENGTH_SHORT).show();
@@ -130,9 +150,25 @@ public class MainActivity extends ActionBarActivity implements AsyncTaskComplete
             startActivity(intent);
             finish();
         } else {
-            username = "";
-            password = "";
-            studentId = "";
+            loggingIn = false;
+            usernameText.setFocusable(true);
+            usernameText.setFocusableInTouchMode(true);
+            usernameText.setClickable(true);
+            usernameText.setEnabled(true);
+            passwordText.setFocusable(true);
+            passwordText.setFocusableInTouchMode(true);
+            passwordText.setClickable(true);
+            passwordText.setEnabled(true);
+            studentIdText.setFocusable(true);
+            studentIdText.setFocusableInTouchMode(true);
+            studentIdText.setClickable(true);
+            studentIdText.setEnabled(true);
+            loginButton.setFocusable(true);
+            loginButton.setFocusableInTouchMode(true);
+            loginButton.setClickable(true);
+            loginButton.setEnabled(true);
+            loginTextLayout.setVisibility(View.VISIBLE);
+            loginLoadingLayout.setVisibility(View.GONE);
             Toast.makeText(this, getString(R.string.toast_login_failed), Toast.LENGTH_SHORT).show();
         }
     }
