ec616b4714c724692c80f000e0d282aea8b03048
Kevin Black <kevin3.black@gmail.com>
2016-02-19 15:04:13 -0600

    Updated versions

--- app/src/main/java/com/manateams/android/manateams/MyApplication.java
+++ app/src/main/java/com/manateams/android/manateams/MyApplication.java
@@ -1,6 +1,9 @@
 package com.manateams.android.manateams;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Logger;
 import com.google.android.gms.analytics.Tracker;
@@ -42,5 +45,11 @@ public class MyApplication extends Application {
 
         }
         return mTrackers.get(trackerId);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
